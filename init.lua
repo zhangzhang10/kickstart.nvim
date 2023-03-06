33b8b11997b102e4cc8c8aab515ed2d75abdbc64
Zhang Zhang <zhang.zhang@amd.com>
2023-03-06 05:50:42 +0000

    Add plugin vim-illuminate

--- init.lua
+++ init.lua
@@ -39,8 +39,8 @@ P.S. You can delete this when you're done too. It's your config now :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = '\\'
-vim.g.maplocalleader = '\\'
+-- vim.g.mapleader = '\\'
+-- vim.g.maplocalleader = '\\'
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
@@ -191,6 +191,9 @@ require('lazy').setup({
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
   },
+
+  -- Highlight symbols under cursor
+  { 'RRethy/vim-illuminate', opts = {} },
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
