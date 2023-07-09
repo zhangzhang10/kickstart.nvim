8cfa15df8d16d80facd6eb30fda912838a78bea7
Zhang Zhang <zhang.zhang@amd.com>
2023-07-09 16:17:45 -0700

    Remove lang servers I don't need

--- init.lua
+++ init.lua
@@ -385,7 +385,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'lua', 'python', 'typescript', 'help', 'vim', 'comment' },
+  ensure_installed = { 'c', 'cpp', 'lua', 'python', 'typescript', 'vim', 'comment' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
