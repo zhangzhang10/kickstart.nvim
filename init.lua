21435dd021079ebcd39b8146018a39390c8d0139
Zhang Zhang <zhang.zhang@amd.com>
2023-09-30 10:15:10 -0700

    Disable treesitter.indentation because it is broken

--- init.lua
+++ init.lua
@@ -399,7 +399,7 @@ require('nvim-treesitter.configs').setup {
   auto_install = false,
 
   -- highlight = { enable = true },
-  indent = { enable = true, disable = { 'python' } },
+  -- indent = { enable = true, disable = { 'python' } },
   incremental_selection = {
     enable = true,
     keymaps = {
