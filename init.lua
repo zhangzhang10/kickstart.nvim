27f6e6ee65448522fdde5337473adf223fd5c4bd
Zhang Zhang <zhang.zhang@amd.com>
2024-10-09 00:34:48 -0700

    Keep copilot.vim and remove copilot.lua

--- init.lua
+++ init.lua
@@ -103,7 +103,7 @@ require('lazy').setup({
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "canary",
     dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "github/copilot.vim" },-- or { "zbirenbaum/copilot.lua" }
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
     build = "make tiktoken", -- Only on MacOS or Linux
